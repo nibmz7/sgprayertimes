1d41d68f25d09840b721d51f16cb19416d041b7c
nibmz7@gmail.com <Kepoh_13>
2018-12-10 23:17:56 +0800

    Initial commit

---
+++ app/src/main/java/com/nibmz7gmail/sgprayertimemusollah/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nibmz7gmail.sgprayertimemusollah
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
